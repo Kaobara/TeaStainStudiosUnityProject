fbbfa4194673da3b0b815a7cf6eacabd6ade1480
stobincouzen <sebastian.tobincouzens@student.unimelb.edu.au>
2022-09-27 10:11:16 +1000

    changed script  name to playermovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {   
     
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update() {
