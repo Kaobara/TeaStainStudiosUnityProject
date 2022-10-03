887300b5b9e50bafc602f8d1d307895d582572bc
Pepu0309 <jasontan000jt@gmail.com>
2022-10-03 19:58:23 +1100

    Added an Options Menu that is in the same scene as the Main Menu. Added functionality of adjusting volumes and saving settings.
    Options Menu has a volume slider which temporarily adjusts the volume of the player's current game session. Should they choose to Apply the settings, then the settings are applied to the current game session, and saved to the player preferences file. Should they choose to Exit the options menu, then the settings will be reverted to what they were before the Player had applied them.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
@@ -8,16 +9,46 @@ using UnityEngine.SceneManagement;
 // MAIN MENU in Unity | All-In-One Tutorial: https://www.youtube.com/watch?v=Cq_Nnw_LwnI
 // Level selection in your Unity game | Unity tutorial: https://www.youtube.com/watch?v=YAHFnF2MRsE
 // Unity SceneManager Documentation: https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.html
+// Unity Slider Documentation: https://docs.unity3d.com/ScriptReference/UIElements.Slider.html (only value was used here)
+// Unity PlayerPrefs Documentation: https://docs.unity3d.com/2020.1/Documentation/ScriptReference/PlayerPrefs.html
 public class MainMenuController : MonoBehaviour
 {   
     [SerializeField] string LevelSelectMenu;
-    [SerializeField] string OptionsMenu;
+    [SerializeField] Slider volumeSlider;
+    private float curSessionVolume;
+
+    void Awake() {
+        // If there is a volume setting found in the player preferences file, then use that value
+        if(PlayerPrefs.HasKey("Volume")) {
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+            AudioListener.volume = volumeSlider.value;
+        // Otherwise use default value of 0.5
+        } else {
+            volumeSlider.value = 0.5f;
+        }
+
+        curSessionVolume = volumeSlider.value;
+    }
 
     public void LoadLevelSelectMenu() {
         SceneManager.LoadScene(LevelSelectMenu);
     }
 
-    public void LoadOptionsMenu() {
-        SceneManager.LoadScene(OptionsMenu);
+    // Set temporary volume as player moves slider around
+    public void SetVolumeTemp() {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    // If player exists the options menu without applying, revert volume change
+    // to the value before they entered the options menu
+    public void RevertVolumeChange() {
+        volumeSlider.value = curSessionVolume;
+        AudioListener.volume = curSessionVolume;
+    }
+
+    // Apply volume changes by saving to player preferences file
+    public void ApplyVolumeChange() {
+        PlayerPrefs.SetFloat("Volume", AudioListener.volume);
+        curSessionVolume = volumeSlider.value;
     }
 }
